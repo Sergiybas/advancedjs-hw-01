7035b41e29dcd934d9242b09ffe6d6ab08dcc157
Sergiybas <163581263+Sergiybas@users.noreply.github.com>
2024-06-08 11:32:33 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,16 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+const gallery = document.querySelector('.gallery');
 
+function galleryBild(arr) {
+  return arr
+    .map(
+      elem =>
+        `<li class="gallery__item">
+            <img class="gallery__image gallery__link" src="${elem.preview}" alt="${elem.description}">
+         </li>`
+    )
+    .join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', galleryBild(galleryItems));
 console.log(galleryItems);
